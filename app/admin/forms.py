5e8788322fe0fa8595334a852618e92ac8da13eb
烧白君 <rayn1027@outlook.com>
2015-12-05 23:20:19 +0800

    update user management

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,16 +1,26 @@
 # coding=utf-8
 from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
+from wtforms import StringField, PasswordField, BooleanField, \
+    SubmitField, SelectField, RadioField
 from wtforms.validators import Length
 from ..utils.validators import MyDataRequired
 from app import DepartmentList
+from app.models import User
 
 
 class LoginForm(Form):
-    username = StringField(u'用户名', validators=[MyDataRequired(), Length(6, 64, message=u'用户名长度在6到64之间')])
+    username = StringField(u'用户名', validators=[MyDataRequired(), Length(2, 64, message=u'用户名长度在6到64之间')])
     password = PasswordField(u'密码', validators=[MyDataRequired()])
     remember_me = BooleanField(u'记住我')
     submit = SubmitField(u'登录')
+
+
+class UserForm(Form):
+    username = StringField(u'用户名', validators=[MyDataRequired(), Length(2, 64, message=u'用户名长度在6到64之间')])
+    password = PasswordField(u'密码', validators=[MyDataRequired()])
+    department = StringField(u'部门', validators=[MyDataRequired()])
+    permission = RadioField(u'权限', choices=[(permission, permission) for permission in User.PermissionList],
+                            default=User.Permission.NORMAL)
 
 
 class AddContestSeriesForm(Form):
--- app/ajax/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-
-ajax = Blueprint('ajax', __name__, static_folder='../static')
-
